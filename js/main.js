e1a7b3b3851735a7f6edfdc319f047a5a5452c3c
Javier Enrique <s0wkete@gmail.com>
2015-02-23 11:10:45 -0300

    base for swipe

--- js/main.js
+++ js/main.js
@@ -78,3 +78,34 @@ $('.mask').css('height',alto);
 $('.mapa-grande').css('height',alto);
 $(".closeSession").css('top', alto - 40);
 
+
+// Swipe
+
+$(function() {
+  //Enable swiping...
+  $("#test").swipe( {
+    //Generic swipe handler for all directions
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      $(this).text("You swiped " + direction );
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+     threshold: 75
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
