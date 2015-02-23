8847bc22d51092944538c6d6c989fa0ddac545f5
Javier Enrique <s0wkete@gmail.com>
2015-02-23 12:39:26 -0300

    new login

--- js/main.js
+++ js/main.js
@@ -80,18 +80,17 @@ $(".closeSession").css('top', alto - 40);
 
 
 // Swipe
-
-$(function() {
-  //Enable swiping...
-  $("#test").swipe( {
-    //Generic swipe handler for all directions
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      $(this).text("You swiped " + direction );
-    },
-    //Default is 75px, set to 0 for demo so any distance triggers swipe
-     threshold: 75
-  });
-});
+// $(function() {
+//   //Enable swiping...
+//   $("#test").swipe( {
+//     //Generic swipe handler for all directions
+//     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+//       $(this).text("You swiped " + direction );
+//     },
+//     //Default is 75px, set to 0 for demo so any distance triggers swipe
+//      threshold: 75
+//   });
+// });
 
 
 
